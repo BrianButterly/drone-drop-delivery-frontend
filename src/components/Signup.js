e12521e3d89befdb6604eec55c066ce745568012
Brian Butterly <butterlybrian@gmail.com>
2021-05-13 21:44:33 -0500

    trying out responsiveness

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,46 +4,48 @@ import { Form, Col, Button } from 'react-bootstrap';
 class Signup extends React.Component {
     render() {
         return (
-            <Form className="signup">
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type="name" placeholder="Name" />
-                    </Form.Group>
-                </Form.Row>
+            <div className="signup">
+                <Form>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control type="name" placeholder="Name" />
+                        </Form.Group>
+                    </Form.Row>
 
-                <Form.Group controlId="formGridAddress1">
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control placeholder="1234 Main St" />
-                </Form.Group>
-
-                <Form.Group controlId="formGridAddress2">
-                    <Form.Label>Address 2</Form.Label>
-                    <Form.Control placeholder="Apartment, studio, or floor" />
-                </Form.Group>
-
-                <Form.Row>
-                    <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control />
+                    <Form.Group controlId="formGridAddress1">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control placeholder="1234 Main St" />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridState">
-                        <Form.Label>State</Form.Label>
-                        <Form.Control />
+                    <Form.Group controlId="formGridAddress2">
+                        <Form.Label>Address 2</Form.Label>
+                        <Form.Control placeholder="Apartment, studio, or floor" />
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridZip">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control />
-                    </Form.Group>
-                </Form.Row>
+                    <Form.Row>
+                        <Form.Group as={Col} controlId="formGridCity">
+                            <Form.Label>City</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridState">
+                            <Form.Label>State</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+
+                        <Form.Group as={Col} controlId="formGridZip">
+                            <Form.Label>Zip</Form.Label>
+                            <Form.Control />
+                        </Form.Group>
+                    </Form.Row>
 
 
-                <Button variant="dark" type="submit">
-                    Submit
+                    <Button variant="dark" type="submit">
+                        Submit
                 </Button>
-            </Form>
+                </Form>
+            </div>
         )
     }
 }
